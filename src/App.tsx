7c7737c96ab2151dfe065589e4d38fb3d2b63481
William With <william.with@hotmail.se>
2022-10-08 16:59:48 +0200

    fixed ionic comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import {IonApp, IonRouterOutlet, IonSplitPane} from '@ionic/react';
+import {IonApp, IonMenuButton, IonRouterOutlet, IonSplitPane, setupIonicReact} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Route } from 'react-router-dom';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Main CSS File */
 import './theme/variables.css';
@@ -9,11 +25,14 @@ import './theme/variables.css';
 import HomePage from './pages/HomePage';
 import AppLayoutMenu from "./components/AppLayoutMenu";
 
+setupIonicReact();
+
 const App = () => {
     return (
     <IonApp>
         <IonReactRouter>
             <IonSplitPane contentId='main'>
+                <IonMenuButton/>
                 <AppLayoutMenu/>
                 <IonRouterOutlet id='main'>
                     <Route>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
-import { IonPage } from "@ionic/react";
+import {IonButton, IonContent, IonPage, IonPopover} from "@ionic/react";
 import React from "react";
 
 const HomePage: React.FC = () => {
     return (
     <IonPage>
         <h1 className='bg-red-600 text-yellow-400'>If this is yellow it mean tailwind is working!</h1>
+        <IonButton id='popover-button'>Pop me!</IonButton>
+        <IonPopover trigger='popover-button' className='bg-blue-300'>
+            <IonContent className='text-6xl font-extrabold text-center text-fuchsia-800'>Hello world!</IonContent>
+        </IonPopover>
     </IonPage>)
 };
 
