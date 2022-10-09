9af3b546b2b66538f73129f7a87dcbaa99ec7b77
william with <william.with@hotmail.se>
2022-10-09 15:59:32 +0200

    build new layout

--- src/components/UICommon/UILogo.tsx
+++ src/components/UICommon/UILogo.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react";
+import React, {FC} from 'react';
 
 // @ts-ignore
 import logo from '../../assets/images/logo.jpg';
--- src/pages/GamesPage.tsx
+++ src/pages/GamesPage.tsx
@@ -1,5 +1,5 @@
-import {IonMenuButton, IonPage} from "@ionic/react";
-import React from "react";
+import {IonMenuButton, IonPage} from '@ionic/react';
+import React from 'react';
 
 
 const GamesPage: React.FC = () => {
