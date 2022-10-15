505f0bcd6a54306572a2b32b33d34034f4d9bcb7
william with <william.with@hotmail.se>
2022-10-15 21:20:45 +0200

    single game page

---
+++ src/util/games.tsx
@@ -0,0 +1,26 @@
+import fallBallIcon from '../assets/images/games/fallball/showcase.png';
+import solitaireIcon from '../assets/images/games/solitaire/showcase.png';
+import videoPokerIcon from '../assets/images/games/videopoker/showcase.png';
+
+const games = [
+    {
+        'image': fallBallIcon,
+        'title': 'Fall Ball',
+        'description': 'Fall ball is a simple, easy and satisfying game.',
+        'genre': 'Casual'
+    },
+    {
+        'image': solitaireIcon,
+        'title': 'Solitaire',
+        'description': 'Solitaire is a relaxing puzzle game.',
+        'genre': 'Puzzle'
+    },
+    {
+        'image': videoPokerIcon,
+        'title': 'Videopoker',
+        'description': 'Videopoker is a relaxing and fun card game.',
+        'genre': 'Puzzle'
+    }
+];
+
+export default games
