6a3880c6eb7ede997867366395066af820f3ee8e
William With <william.with@hotmail.se>
2022-10-24 09:00:41 +0200

    fix contact page + align youa re hwere center

--- src/components/menu/MenuYouAreHere.tsx
+++ src/components/menu/MenuYouAreHere.tsx
@@ -3,7 +3,7 @@ import {GoLocation} from 'react-icons/go';
 
 
 const MenuYouAreHere: React.FC = () => {
-    return <div className='text-xs text-red-500 flex items-center uppercase font-roboto'>
+    return <div className='text-xs text-red-500 flex items-center justify-center uppercase font-roboto'>
         <div className='px-1'>You Are Here</div>
         <GoLocation/>
     </div>
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -11,7 +11,7 @@ const ContactPage: React.FC = () => {
                 <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim rutrum justo ut viverra. Phasellus tortor turpis, dignissim quis sollicitudin et, malesuada porta eros. Quisque sit amet mollis quam. Aenean ornare, neque ut vestibulum lobortis, augue justo rhoncus orci, vel lobortis ex sem sed velit. Quisque condimentum luctus nisi quis fermentum. Nam eget felis et nisl venenatis vulputate vitae eu ex. Maecenas condimentum porta gravida. Nam scelerisque aliquet risus, quis pulvinar justo. Ut vitae pellentesque tellus, non efficitur leo. Vestibulum ut magna quam. Fusce sit amet nunc vestibulum, faucibus sem ut, malesuada odio. </span>
             </div>
             <a href='mailto:gamebaconstudios.com'
-                className='font-roboto font-medium mt-10 items-center text-lg md:text-2xl flex
+                className='font-roboto mb-12 font-medium mt-10 items-center text-lg md:text-2xl flex
                 w-fit shadow-menu-item rounded-4xl p-5 mx-auto'>
                 <FiMail className='text-lg md:text-3xl mr-5'/>
                     gamebaconstudios@gmail.com
