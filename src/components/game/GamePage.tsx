88fb06d42625a849634daaa11c689e4a927344bd
William With <william.with@hotmail.se>
2022-11-15 22:30:01 +0100

    test

--- src/components/game/GamePage.tsx
+++ src/components/game/GamePage.tsx
@@ -7,15 +7,16 @@ import {IonPage, IonBackButton, IonRouterLink} from '@ionic/react';
 const GamePage: React.FC = () => {
     const {id} = useParams<any>();
     const game = games[id];
-    return <IonPage className=''>
+    console.log(`detail url: >${game.image}<`)
+    return <IonPage>
         <IonBackButton defaultHref='/games' className='absolute p-5' mode='md'/>
         <div className='justify-center flex mt-20'>
             <div className='flex flex-col m-5 shadow-2xl rounded-2xl w-fit'>
-                <img src={game?.image} className='w-40 md:w-96 rounded-t-2xl'/>
+                <img src={game.image} className='w-40 md:w-96 rounded-t-2xl'/>
                 <div className='rounded-r-2xl flex flex-col p-5 pb-3'>
                     <div className=''>
-                        <h2 className='font-semibold text-2xl'>{game?.title}</h2>
-                        <span className=''>{game?.description}</span>
+                        <h2 className='font-semibold text-2xl'>{game.title}</h2>
+                        <span className=''>{game.description}</span>
                     </div>
                     <div className='justify-between flex text-xs font-thin mt-10'>
                         <IonRouterLink>About</IonRouterLink>
